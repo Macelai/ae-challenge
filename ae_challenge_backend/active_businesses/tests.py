5f3ed74a5d529f00b877823d6af4e0b65569f7ac
Vinicius Macelai <vmacelai@esss.co>
2020-12-11 17:55:24 -0300

    test: add test to get business with most location

--- ae_challenge_backend/active_businesses/tests.py
+++ ae_challenge_backend/active_businesses/tests.py
@@ -1,3 +1,15 @@
+from rest_framework import status
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+class TestActiveBusinesses(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_get_business_with_most_location(self):
+        response = self.client.get(reverse("business_most_location"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["business"], "SP PLUS CORPORATION")
+        self.assertEqual(response.data["value"], 74)
--- ae_challenge_backend/active_businesses/urls.py
+++ ae_challenge_backend/active_businesses/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("api/business_most_location/", views.business_most_location),
+    path(
+        "api/business_most_location/",
+        views.business_most_location,
+        name="business_most_location",
+    ),
 ]
