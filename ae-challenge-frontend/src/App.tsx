28d042945654753dc2ef1029829ccbc5d5b7508b
Vinicius Macelai <vmacelai@esss.co>
2020-12-12 18:17:30 -0300

    feat: add page with button to fetch data from API

--- ae-challenge-frontend/src/App.tsx
+++ ae-challenge-frontend/src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState({business: '', date: ''});
+
+  const sendRequest = () =>{
+    fetch(
+      `http://127.0.0.1:8000/api/oldest_location/`
+    )
+    .then(res => res.json())
+      .then(res => {
+        setState(res);
+      })
+      .catch(error => console.log(error));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {state.business} <br></br>
+        {state.date}
+
+        <button onClick={sendRequest}>Click me</button>
       </header>
     </div>
   );
